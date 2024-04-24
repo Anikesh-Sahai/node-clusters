04df011cd938f3541ba136b2523a83fa31ed5973
Anikesh Sahai <anikeshsahai.shopify878@webkul.in>
2024-04-24 16:00:14 +0530

    cluster forking acoording to the cores

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
+process.env.UV_THREADPOOL_SIZE=1
+
 const cluster = require("cluster");
 
-const crypto = require('crypto')
 
 
 // is file is being executed in cluster mode?
 if (cluster.isMaster) {
   // cause index file to be run again child mode
   cluster.fork()
-  // cluster.fork()
-  // cluster.fork()
-  // cluster.fork()
+  cluster.fork()
 } else {
+  const crypto = require('crypto')
+
   // i am a child i am going to act like a server and do nothing else
   const express = require("express");
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {
-      /* just loop */
-    }
-  }
 
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there.");
+    crypto.pbkdf2('a','b',100000,512,'sha512',()=>{
+      res.send("Hi there.");
+    })
   });
 
   app.get("/fast", (req, res) => {
@@ -33,3 +29,8 @@ if (cluster.isMaster) {
 
   app.listen(3000);
 }
+
+
+
+// ab -c 50 -n 500 localhost:3000/fast
+// for checking the bench  marking of our /fast route -c 50 means 50 req must be present at a moment and -n 500 represents 500 request to be send
